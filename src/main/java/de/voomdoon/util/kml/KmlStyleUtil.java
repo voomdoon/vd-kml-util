ddd048798ded6720b2ccbcfa5bb5474de580fdf8
voomdoon <nilreb24-github20141105@yahoo.de>
2023-08-06 18:56:12 +0200

    feat: add setStyle

---
+++ src/main/java/de/voomdoon/util/kml/KmlStyleUtil.java
@@ -0,0 +1,61 @@
+package de.voomdoon.util.kml;
+
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Style;
+import de.micromata.opengis.kml.v_2_2_0.StyleSelector;
+import lombok.experimental.UtilityClass;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+@UtilityClass
+public class KmlStyleUtil {
+	/**
+	 * DOCME add JavaDoc for method setStyle
+	 * 
+	 * @param placemark
+	 * @param style
+	 * @param document
+	 * @since 0.1.0
+	 */
+	public static void setStyle(Placemark placemark, Style style, Document document) {
+		add(document, style);
+		placemark.setStyleUrl(getUrl(style));
+	}
+
+	/**
+	 * @param document
+	 *            {@link Document}
+	 * @param style
+	 *            {@link Style}
+	 * @since 0.2.0
+	 */
+	private static void add(Document document, Style style) {
+		boolean found = false;
+
+		for (StyleSelector ss : document.getStyleSelector()) {
+			if (ss.equals(style)) {
+				found = true;
+			}
+		}
+
+		if (!found) {
+			document.getStyleSelector().add(style);
+		}
+	}
+
+	/**
+	 * @param style
+	 *            {@link Style}
+	 * @return URL {@link String}
+	 * @since 0.2.0
+	 */
+	private static String getUrl(Style style) {
+		return "#" + style.getId();
+	}
+}
---
+++ src/test/java/de/voomdoon/util/kml/KmlStyleUtilTest.java
@@ -0,0 +1,89 @@
+package de.voomdoon.util.kml;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+import de.micromata.opengis.kml.v_2_2_0.Document;
+import de.micromata.opengis.kml.v_2_2_0.Placemark;
+import de.micromata.opengis.kml.v_2_2_0.Style;
+import de.voomdoon.testing.logging.tests.LoggingCheckingTestBase;
+
+/**
+ * DOCME add JavaDoc for
+ *
+ * @author André Schulz
+ *
+ * @since 0.1.0
+ */
+class KmlStyleUtilTest {
+
+	/**
+	 * DOCME add JavaDoc for KmlStyleUtilTest
+	 *
+	 * @author André Schulz
+	 *
+	 * @since 0.1.0
+	 */
+	static class SetStyleUrlTest extends LoggingCheckingTestBase {
+
+		/**
+		 * DOCME add JavaDoc for method test
+		 * 
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_Placemark1_Document_styleSelector() throws Exception {
+			logTestStart();
+
+			Document document = new Document();
+			Placemark placemark = new Placemark();
+			Style style = new Style();
+			style.setId("test");
+
+			KmlStyleUtil.setStyle(placemark, style, document);
+
+			assertThat(document.getStyleSelector()).hasSize(1);
+		}
+
+		/**
+		 * DOCME add JavaDoc for method test
+		 * 
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_Placemark1_Placemark_styleUrl() throws Exception {
+			logTestStart();
+
+			Document document = new Document();
+			Placemark placemark = new Placemark();
+			Style style = new Style();
+			style.setId("test");
+
+			KmlStyleUtil.setStyle(placemark, style, document);
+
+			assertThat(placemark.getStyleUrl()).isEqualTo("#test");
+		}
+
+		/**
+		 * DOCME add JavaDoc for method test
+		 * 
+		 * @since 0.1.0
+		 */
+		@Test
+		void test_Placemark2_Document_styleSelector() throws Exception {
+			logTestStart();
+
+			Document document = new Document();
+			Placemark placemark1 = new Placemark();
+			Placemark placemark2 = new Placemark();
+			Style style = new Style();
+			style.setId("test");
+
+			KmlStyleUtil.setStyle(placemark1, style, document);
+			KmlStyleUtil.setStyle(placemark2, style, document);
+
+			assertThat(document.getStyleSelector()).hasSize(1);
+		}
+	}
+}
